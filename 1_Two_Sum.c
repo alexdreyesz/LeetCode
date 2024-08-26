a3ad4e89d5617bff61b5967d0c3a5d40b53d4832
alexdreyesz <alexdreyesz@gmail.com>
2024-08-25 23:47:26 -0400

    1.Two Sum

--- 1_Two_Sum.c
+++ 1_Two_Sum.c
@@ -2,33 +2,42 @@
 #include <stdlib.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2; 
+    int* result = (int*)malloc(2 * sizeof(int));
+    
     for(int i = 0; i < numsSize; i++) {
         for(int j = i + 1; j < numsSize; j++) {
             if((nums[i] + nums[j]) == target) {
-                returnSize[0] = i;
-                returnSize[1] = j;
-                return returnSize;
+                result[0] = i;
+                result[1] = j;
+                return result; 
             }
         }
     }
+    
+    return NULL; 
 }
 
-int main () {
+int main() {
     int numsSize = 4;
-    int ResultSize = 2;
-    int *nums = (int*)malloc(numsSize * sizeof(int));
-    nums[0] = 3;
-    nums[1] = 2;
-    nums[2] = 4;
-    nums[3] = 15;
+    int returnSize = 0;
+    int nums[] = {2, 7, 11, 15};
     int target = 9;
-    int *returnSize = (int*)malloc(ResultSize * sizeof(int));
+    int *returnResult = (int*)malloc(returnSize * sizeof(int));
 
-    returnSize = twoSum(nums, numsSize, target, returnSize);
+    returnResult = twoSum(nums, numsSize, target, &returnSize);
 
-    for (int i = 0; i < ResultSize; i++) {
-        printf("[%d],", returnSize[i]);
+    if (returnResult != NULL) {
+        for (int i = 0; i < returnSize; i++) {
+            printf("[%d]", returnResult[i]);
+        }
+    } else {
+        printf("No Two Numbers Sum Up To The Target.\n");
     }
-    
+
+    printf("\n");
+
+    free(returnResult);
+
     return 0; 
 }
