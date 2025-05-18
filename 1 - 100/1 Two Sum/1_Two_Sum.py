917ef727167fe2f93e0d93cce7df842893ca0883
alexdreyesz <alexdreyesz@gmail.com>
2025-05-18 17:54:05 -0400

    1. Two Sum

---
+++ 1 - 100/1 Two Sum/1_Two_Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_map = {}
+
+        for i, number in enumerate(nums):
+            complement = target - number
+            
+            if complement in my_map:
+                return [my_map[complement], i]
+            
+            my_map[number] = i
